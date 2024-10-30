959bd24f13a3ff2539ba8781722cfa42e540da03
xefade <108057292+xefade@users.noreply.github.com>
2024-10-30 10:55:33 +0700

    Add file

---
+++ BUContext.js
@@ -0,0 +1,84 @@
+const ca_img = "https://raw.githubusercontent.com/xefade/iHaveIdea/refs/heads/main/src/assets/1_CA2-removebg.png?token=GHSAT0AAAAAACZO2ZCJF62XSNS37ZQKGZN6ZZBBJ6A";
+const fi_img = "https://raw.githubusercontent.com/xefade/iHaveIdea/refs/heads/main/src/assets/2_FI-removebg.png?token=GHSAT0AAAAAACZO2ZCIY6VFZV7KAR7VNG3GZZBBKUQ";
+const cgs_img = "https://raw.githubusercontent.com/xefade/iHaveIdea/refs/heads/main/src/assets/3_CGS-removebg.png?token=GHSAT0AAAAAACZO2ZCJGZN3C2DZDCBUE6R4ZZBBLHA";
+const d_r_img = "https://raw.githubusercontent.com/xefade/iHaveIdea/refs/heads/main/src/assets/4_D_R2-removebg.png?token=GHSAT0AAAAAACZO2ZCIGYHOJKTCCZ3IZZ6AZZBBLWQ";
+const scgc_img = "https://raw.githubusercontent.com/xefade/iHaveIdea/refs/heads/main/src/assets/6_SCGC2-removebg.png?token=GHSAT0AAAAAACZO2ZCIRZEKF7WOHX63RBI6ZZBBMFQ";
+const scgcl_img = "https://raw.githubusercontent.com/xefade/iHaveIdea/refs/heads/main/src/assets/7_SCGCL2-removebg.png?token=GHSAT0AAAAAACZO2ZCJONJCPSSGZ2DOWVQCZZBBMPQ";
+const scgp_img = "https://raw.githubusercontent.com/xefade/iHaveIdea/refs/heads/main/src/assets/8_SCGP2-removebg.png?token=GHSAT0AAAAAACZO2ZCJUNMXRZJVJ46HJIR6ZZBBNAQ";
+import scgd_img from "./9_SCGD2-removebg.png"; 
+const sjwd_img = "https://raw.githubusercontent.com/xefade/iHaveIdea/refs/heads/main/src/assets/10_SJWD2-removebg.png?token=GHSAT0AAAAAACZO2ZCIBAHJE42OUNTWFVR6ZZBBP4Q";
+const sl_img = "https://raw.githubusercontent.com/xefade/iHaveIdea/refs/heads/main/src/assets/11_SL-removebg.png?token=GHSAT0AAAAAACZO2ZCIQSVVRGHA76V2LRASZZBBQJA";
+
+export const card_data = [
+
+    {
+        _id: "1",
+        bu_name: "CA",
+        image: ca_img,
+        idea_num: "132"
+    },
+
+    {
+        _id: "2",
+        bu_name: "FI",
+        image: fi_img,
+        idea_num: "46"
+    },
+
+    {
+        _id: "3",
+        bu_name: "CGS",
+        image: cgs_img,
+        idea_num: "751"
+    },
+
+    {
+        _id: "4",
+        bu_name: "D&R",
+        image: d_r_img,
+        idea_num: "54"
+    },
+
+    {
+        _id: "5",
+        bu_name: "SCGC",
+        image: scgc_img,
+        idea_num: "12"
+    },
+
+    {
+        _id: "6",
+        bu_name: "SCGCL",
+        image: scgcl_img,
+        idea_num: "647"
+    },
+
+    {
+        _id: "7",
+        bu_name: "SCGP",
+        image: scgp_img,
+        idea_num: "825"
+    },
+
+    {
+        _id: "8",
+        bu_name: "SCGD",
+        image: scgd_img,
+        idea_num: "543"
+    },
+
+    {
+        _id: "9",
+        bu_name: "SJWD",
+        image: sjwd_img,
+        idea_num: "76"
+    },
+    
+    {
+        _id: "10",
+        bu_name: "SL",
+        image: sl_img,
+        idea_num: "46"
+    }
+
+];
